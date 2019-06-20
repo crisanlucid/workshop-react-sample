1c7bbed47539674124f711fe669b0e5aedac1ac0
crisanlucid <crisan.lucid@yahoo.com>
2019-06-20 17:16:15 +0200

    husky now()

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component {
     console.log('sss_2');
     console.log('sss_3');
     console.log('sss_4');
+    console.log('sss_5');
     // app.post('/api/v1/superheroes', (req, res) => {}
     let url = 'http://localhost:8000/api/v1/superheroes';
 
