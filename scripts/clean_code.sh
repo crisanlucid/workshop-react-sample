4c08926b89442d7035b6a8d0fb739e7870e4ecd5
crisanlucid <crisan.lucid@yahoo.com>
2019-06-21 11:32:37 +0200

    code clean

--- scripts/clean_code.sh
+++ scripts/clean_code.sh
@@ -58,3 +58,5 @@ FORBIDDEN='console.log'
 git diff --cached --name-only | \
     grep -E $FILES_PATTERN | \
     GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo "COMMIT REJECTED Found $FORBIDDEN references. Please remove them before commiting" && exit 1
+
+echo "END"
