ae3d9a183dfb7f14c61df02e5b73ad3cf332d02d
crisanlucid <crisan.lucid@yahoo.com>
2019-06-25 12:58:18 +0200

    - config React Redux, install deps into project - create Component Authorisation, Profile - work with Redux in case you Submit the Form from the Authorisation Component - Show in Profile the user taking from the Store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducer from './store/reducers';
 
+import Authorisation from './Authorisation';
+import Profile from './Profile';
+
+const store = createStore(reducer);
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <button id='Button-click' />
-      </div>
+      <Provider store={store}>
+        <div>
+          <div>
+            <Authorisation />
+          </div>
+          <div>
+            <Profile />
+          </div>
+        </div>
+      </Provider>
     );
   }
 }
